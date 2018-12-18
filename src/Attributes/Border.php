08e943364258976b52c9641accdaf7217f4eb10e
Martijn Kriek <martijn.kriek@relax-software.nl>
2018-12-19 00:50:01 +0100

    Moved Border consts to php-stylesheets project, renamed translator class.

---
+++ src/Attributes/Border.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Relaxsd\Stylesheets\Attributes;
+
+interface Border
+{
+
+    const BORDER = 'border';
+    const BORDER_LEFT = 'border-bottom';
+    const BORDER_RIGHT = 'border-bottom';
+    const BORDER_TOP = 'border-bottom';
+    const BORDER_BOTTOM = 'border-bottom';
+
+}
