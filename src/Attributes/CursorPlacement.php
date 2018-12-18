1bcf959acbe0bde87f4b411e74f4c0d78e50f467
Martijn Kriek <martijn.kriek@relax-software.nl>
2018-12-19 00:00:41 +0100

    Added CursorPlacement and MultiLine attributes to remove dependency on FPdf implementation.

---
+++ src/Attributes/CursorPlacement.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Relaxsd\Stylesheets\Attributes;
+
+interface CursorPlacement
+{
+    const ATTRIBUTE = 'cursor-placement';
+
+    const CURSOR_TOP_RIGHT = 'top-right';
+    const CURSOR_TOP_LEFT = 'top-left';
+    const CURSOR_BOTTOM_RIGHT = 'bottom-right';
+    const CURSOR_BOTTOM_LEFT = 'bottom-left';
+
+    const CURSOR_NEWLINE = 'newline';
+
+}
---
+++ src/Attributes/MultiLine.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Relaxsd\Stylesheets\Attributes;
+
+interface Multiline
+{
+    const ATTRIBUTE = 'multiline';
+
+    const ON = true;
+    const OFF = false;
+
+}
