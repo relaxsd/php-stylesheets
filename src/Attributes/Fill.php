558b47055e6a23a2e135db19be9c6abd3fb37dfd
Martijn Kriek <martijn.kriek@relax-software.nl>
2018-12-19 01:08:10 +0100

    Moved Fill consts to php-stylesheets project, renamed translator class.

---
+++ src/Attributes/Fill.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Relaxsd\Stylesheets\Attributes;
+
+interface Fill
+{
+    const ATTRIBUTE = 'fill';
+
+    const YES = true;
+    const NO = false;
+
+}
---
+++ src/Attributes/FillColor.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Relaxsd\Stylesheets\Attributes;
+
+interface FillColor
+{
+    const ATTRIBUTE = 'fill-color';
+
+}
